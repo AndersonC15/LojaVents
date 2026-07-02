c20a0f1b4159e64e290fa5f05782f908d912d579
AndersonC15 <coellojanderson@gmail.com>
2026-07-02 08:17:29 -0500

    Modificar Controladores para empezar por la APIRest

--- src/main/java/com/lojavents/api/controller/AuthControlador.java
+++ src/main/java/com/lojavents/api/controller/AuthControlador.java
@@ -1,4 +1,13 @@
 package com.lojavents.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthControlador {
+
+    @GetMapping("/")
+    public String inicio() {
+        return "Backend LojaVents funcionando correctamente";
+    }
 }
